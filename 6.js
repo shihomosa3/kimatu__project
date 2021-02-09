74e4c4447bad94aa2bf0b87418bf4218a14e2061
shihomosa3 <zm18535@josai.ac.jp>
2021-02-09 16:29:05 +0000

    2-10-1

--- 6.js
+++ 6.js
@@ -2,7 +2,8 @@ function txtchange1() {
 document.getElementById('txt1').innerHTML = 'ナンバーワン！';
 }
 function imgchange1() {
-document.getElementById('logo').src = './image/bulbon.gif';
+document.getElementById('logo').src ='./image/bulbon.gif';
+
 }
 function show_innerHTMLtxt1(){
 var result1 = document.getElementById('txt1').innerHTML;
--- 9.js
+++ 9.js
@@ -1,3 +1,13 @@
 function sayhello71(){
-alert('Hello,'+ document.getElementById('who71').value +'!')
+  alert('Hello,'+
+       document.getElementById('who71').value+'!');
+}
+function ThreeSecChange(){
+
+  setInterval("imgchange12()",3000);}
+
+
+function imgchange12(){
+
+  document.getElementById('logo2').src ='./image/bulbon.gif';
 }
